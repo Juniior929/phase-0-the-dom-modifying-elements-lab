b7ea43348bf41048c48a84b0b59ba692cf0fa8a2
+ <+>
2023-08-22 20:57:58 -0400

    dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainNode = document.querySelector('#main');
+mainNode.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = 'George'; 
+newHeader.textContent = `${yourName} is the champion`;
+document.body.appendChild(newHeader);
